0b9b67dec93b77c1145029cff3d091dbf6b42187
ElPanaHans <Juanp4blog@gmail.com>
2024-09-27 13:45:38 -0600

    add "Our band Gallery"

--- src/Pages/Gallery.jsx
+++ src/Pages/Gallery.jsx
@@ -5,7 +5,7 @@ import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
 import { useTranslation } from "react-i18next";
 
 export default function Gallery() {
-  const {t} = useTranslation('gallery')
+  const { t } = useTranslation("gallery");
   const slidesFotografia = [
     { url: "https://i.ibb.co/zSLF0dj/Fotografia-1.webp" },
     { url: "https://i.ibb.co/ykBYNvv/Fotografia-2.webp" },
@@ -63,10 +63,34 @@ export default function Gallery() {
     { url: "https://i.ibb.co/pLJRMKd/Pasillo-2.webp " },
     { url: "https://i.ibb.co/zJXqTsM/soda.webp " },
   ];
+  const slidesBanda = [
+    { url: "https://i.ibb.co/z6zyH8b/Banda-12.webp" },
+    { url: "https://i.ibb.co/tzfJCbv/Banda-13.webp" },
+    { url: "https://i.ibb.co/BcQ9pLN/Banda-14.webp" },
+    { url: "https://i.ibb.co/Z1wWRxx/Banda-15.webp" },
+    { url: "https://i.ibb.co/m5hG22T/Banda-16.webp" },
+    { url: "https://i.ibb.co/KytJVCP/Banda-17.webp" },
+    { url: "https://i.ibb.co/2hKMHwT/Banda-18.webp" },
+    { url: "https://i.ibb.co/3TpZ1SW/Banda-19.webp" },
+    { url: "https://i.ibb.co/9vSCXjh/Banda-20.webp" },
+    { url: "https://i.ibb.co/dLs9Xbt/Banda-21.webp" },
+    { url: "https://i.ibb.co/dcYqWbV/Banda-1.webp" },
+    { url: "https://i.ibb.co/YD30zrp/Banda-2.webp" },
+    { url: "https://i.ibb.co/LZvQ9dg/Banda-3.webp" },
+    { url: "https://i.ibb.co/CngdWmj/Banda-4.webp" },
+    { url: "https://i.ibb.co/kmzd0qN/Banda-5.webp" },
+    { url: "https://i.ibb.co/yNx7P9b/Banda-6.webp" },
+    { url: "https://i.ibb.co/gRSzjVS/Banda-7.webp" },
+    { url: "https://i.ibb.co/m9Y08th/Banda-8.webp" },
+    { url: "https://i.ibb.co/9pHdFS8/Banda-9.webp" },
+    { url: "https://i.ibb.co/jRxt9ZM/Banda-10.webp" },
+    { url: "https://i.ibb.co/Ng0F2Ds/Banda-11.webp" },
+  ];
   const [currentIndexArte, setCurrentIndexArte] = useState(0);
   const [currentIndexFotografia, setCurrentIndexFotografia] = useState(0);
   const [currentIndexEsculturas, setCurrentIndexEsculturas] = useState(0);
   const [currentIndexExtras, setCurrentIndexExtras] = useState(0);
+  const [currentIndexBanda, setCurrentIndexBanda] = useState(0);
 
   //Manejo de botones de Arte
 
@@ -126,10 +150,28 @@ export default function Gallery() {
     const newIndex = isLastSlide ? 0 : currentIndexExtras + 1;
     setCurrentIndexExtras(newIndex);
   };
+
+  
+  //Manejo de botones de Banda
+
+  const prevSlideBanda = () => {
+    const isFirstSlide = currentIndexBanda === 0;
+    const newIndex = isFirstSlide
+      ? slidesBanda.length - 1
+      : currentIndexBanda - 1;
+    setCurrentIndexBanda(newIndex);
+  };
+  const nextSlideBanda = () => {
+    const isLastSlide = currentIndexBanda === slidesBanda.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndexBanda + 1;
+    setCurrentIndexBanda(newIndex);
+  };
   return (
     <Suspense fallback="Cargando Galeria">
       <>
-      <h1 className="text-Sycamore font-bold text-center text-3xl mb-12">{t("title")}</h1>
+        <h1 className="mb-12 text-center text-3xl font-bold text-Sycamore">
+          {t("title")}
+        </h1>
         <section className="relative m-auto flex w-[90%] grid-cols-2 flex-col content-center items-center justify-center gap-8 md:grid">
           <div className="group relative flex flex-col items-center justify-center gap-8">
             <h1 className="text-pretty text-center text-2xl font-bold text-Atlantis lg:text-3xl">
@@ -139,7 +181,7 @@ export default function Gallery() {
               style={{
                 backgroundImage: `url(${slidesArte[currentIndexArte].url})`,
               }}
-              className="aspect-square h-[400px] rounded-xl bg-cover bg-center duration-500 md:h-[90%] w-[90%] lg:w-[60%]"
+              className="aspect-square h-[400px] w-[90%] rounded-xl bg-cover bg-center duration-500 md:h-[90%] lg:w-[60%]"
             ></div>
 
             <div className="absolute left-4 top-[50%] -translate-x-0 -translate-y-[-50%] cursor-pointer rounded-full bg-black/20 p-2 text-2xl text-White group-hover:block md:left-8 lg:left-48 lg:hidden">
@@ -153,14 +195,14 @@ export default function Gallery() {
 
           <div className="group relative flex flex-col items-center justify-center gap-8">
             <h1 className="text-pretty text-center text-2xl font-bold text-Atlantis lg:text-3xl">
-            {t("photography")}
+              {t("photography")}
             </h1>
 
             <div
               style={{
                 backgroundImage: `url(${slidesFotografia[currentIndexFotografia].url})`,
               }}
-              className="aspect-square h-[400px] rounded-xl bg-cover bg-center duration-500 md:h-[90%] w-[90%] lg:w-[60%]"
+              className="aspect-square h-[400px] w-[90%] rounded-xl bg-cover bg-center duration-500 md:h-[90%] lg:w-[60%]"
             ></div>
 
             <div className="absolute left-4 top-[50%] -translate-x-0 -translate-y-[-50%] cursor-pointer rounded-full bg-black/20 p-2 text-2xl text-White group-hover:block md:left-8 lg:left-48 lg:hidden">
@@ -174,14 +216,14 @@ export default function Gallery() {
 
           <div className="group relative flex flex-col items-center justify-center gap-8">
             <h1 className="text-pretty text-center text-2xl font-bold text-Atlantis lg:text-3xl">
-            {t("sculptures")}
+              {t("sculptures")}
             </h1>
 
             <div
               style={{
                 backgroundImage: `url(${slidesEsculturas[currentIndexEsculturas].url})`,
               }}
-              className="aspect-square h-[400px] rounded-xl bg-cover bg-center duration-500 md:h-[90%] w-[90%] lg:w-[60%]"
+              className="aspect-square h-[400px] w-[90%] rounded-xl bg-cover bg-center duration-500 md:h-[90%] lg:w-[60%]"
             ></div>
             <div className="absolute left-4 top-[50%] -translate-x-0 -translate-y-[-50%] cursor-pointer rounded-full bg-black/20 p-2 text-2xl text-White group-hover:block md:left-8 lg:left-48 lg:hidden">
               <BsChevronCompactLeft onClick={prevSlideEsculturas} size={30} />
@@ -193,14 +235,14 @@ export default function Gallery() {
           </div>
           <div className="group relative flex flex-col items-center justify-center gap-8">
             <h1 className="text-pretty text-center text-2xl font-bold text-Atlantis lg:text-3xl">
-            {t("installations")}
+              {t("installations")}
             </h1>
 
             <div
               style={{
                 backgroundImage: `url(${slidesExtras[currentIndexExtras].url})`,
               }}
-              className="aspect-square h-[400px] rounded-xl bg-cover bg-center duration-500 md:h-[90%] w-[90%] lg:w-[60%]"
+              className="aspect-square h-[400px] w-[90%] rounded-xl bg-cover bg-center duration-500 md:h-[90%] lg:w-[60%]"
             ></div>
             <div className="absolute left-4 top-[50%] -translate-x-0 -translate-y-[-50%] cursor-pointer rounded-full bg-black/20 p-2 text-2xl text-White group-hover:block md:left-8 lg:left-48 lg:hidden">
               <BsChevronCompactLeft onClick={prevSlideExtras} size={30} />
@@ -208,6 +250,27 @@ export default function Gallery() {
 
             <div className="absolute right-4 top-[50%] -translate-x-0 -translate-y-[-50%] cursor-pointer rounded-full bg-black/20 p-2 text-2xl text-White group-hover:block md:right-8 lg:right-48 lg:hidden">
               <BsChevronCompactRight onClick={nextSlideExtras} size={30} />
+            </div>
+          </div>
+
+
+          <div className="group relative flex flex-col items-center justify-center gap-8">
+            <h1 className="text-pretty text-center text-2xl font-bold text-Atlantis lg:text-3xl">
+              {t("band")}
+            </h1>
+
+            <div
+              style={{
+                backgroundImage: `url(${slidesBanda[currentIndexBanda].url})`,
+              }}
+              className="aspect-square h-[400px] w-[90%] rounded-xl bg-cover bg-center duration-500 md:h-[90%] lg:w-[60%]"
+            ></div>
+            <div className="absolute left-4 top-[50%] -translate-x-0 -translate-y-[-50%] cursor-pointer rounded-full bg-black/20 p-2 text-2xl text-White group-hover:block md:left-8 lg:left-48 lg:hidden">
+              <BsChevronCompactLeft onClick={prevSlideBanda} size={30} />
+            </div>
+
+            <div className="absolute right-4 top-[50%] -translate-x-0 -translate-y-[-50%] cursor-pointer rounded-full bg-black/20 p-2 text-2xl text-White group-hover:block md:right-8 lg:right-48 lg:hidden">
+              <BsChevronCompactRight onClick={nextSlideBanda} size={30} />
             </div>
           </div>
         </section>
